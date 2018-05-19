e0be086aa8e31f9bdd78b75815f7308284e86834
Markus Häll <soundgoof@gmail.com>
2018-05-19 15:49:22 +0200

    added mac 2 redis insert

--- dhcp-hook.py
+++ dhcp-hook.py
@@ -7,15 +7,18 @@ import sqlite3
 db = redis.Redis()
 
 if sys.argv[1] == "commit":
-  ip = sys.argv[2]
-  sw = sys.argv[3]
-  relay = sys.argv[5]
-  db.set(ip, sw)
-  if relay != '0.0.0.0':
-    db.set('relay-%s' % ip, relay)
+  swMac = sys.argv[2]
+  swIp = sys.argv[3]
+  swName = sys.argv[4]
+  swClient = sys.argv[5]
+  swRelay = sys.argv[6]
+  db.set(swIp, swName)
+  db.set('mac-{}'.format(swMac), swIp)
+  if swRelay != '0.0.0.0':
+    db.set('relay-{}'.format(SwIp), swRelay)
     sqlidb = sqlite3.connect('/etc/ipplan.db')
     cursor = sqlidb.cursor()
     sql = "SELECT short_name FROM network WHERE ipv4_gateway_txt = ?"
-    networkname = cursor.execute(sql, (relay, )).fetchone()[0]
-    db.set('networkname-%s' % ip, networkname)
-  os.system("/scripts/swboot/configure " + ip + " &")
+    networkname = cursor.execute(sql, (swRelay, )).fetchone()[0]
+    db.set('networkname-{}'.format(swIp), networkname)
+  os.system("/scripts/swboot/configure " + swIp + " &")
